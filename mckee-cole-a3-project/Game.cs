8ac61fa45c541646a229f437ef25dcd40d0af56d
ColeM-Mohawk <000983139@mohawkcollege.ca>
2026-03-23 20:47:02 -0400

    Add player class

---
+++ mckee-cole-a3-project/Game.cs
@@ -0,0 +1,48 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+        Vector2 position;
+
+        float secondsElapsed = Time.SecondsElapsed;
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("Holder");
+            Window.SetSize(800, 600);
+
+
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            Window.ClearBackground(Color.OffWhite);
+
+
+            
+
+            position += Vector2.Zero * 100 * Time.DeltaTime;
+
+            Draw.FillColor = Color.Red;
+            Draw.LineColor = Color.Black;
+            Draw.Square(100, 800, 80);
+           
+        }
+    }
+
+}
---
+++ mckee-cole-a3-project/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace MohawkGame2D
+{
+    public class Player
+    {
+        public Vector2 input = Vector2.Zero;
+
+        public void GetPlayerInput()
+        {
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Up) || Input.IsKeyboardKeyDown(KeyboardInput.W))
+            {
+                input.Y -= 1;
+            }
+
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Down) || Input.IsKeyboardKeyDown(KeyboardInput.S))
+            {
+                input.Y += 1;
+            }
+
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Left) || Input.IsKeyboardKeyDown(KeyboardInput.A))
+            {
+                input.X -= 1;
+            }
+
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Right) || Input.IsKeyboardKeyDown(KeyboardInput.D))
+            {
+                input.X += 1;
+            }
+        }
+    }
+}
